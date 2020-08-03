9665233467eaa450d351edd4ad9799423779a8cf
gpb8902 <65796885+gpb8902@users.noreply.github.com>
2020-08-04 00:33:52 +0900

    Update 20200803.py

--- 20200803.py
+++ 20200803.py
@@ -1,25 +1,16 @@
-from random import *
-date = randint(4, 28)
-print("오프라인 스터디 모임 날짜는 매월" + str(date) + "일로 선정되었습니다.")
+jumin = "200314-2315031"
 
-sentence = '나는 내일 전역하는 개꿀빤 입대한지 7일된 이등병, 李燈昞(오얏나무처럼 크고 등잔처럼 밝아라 라는 뜻의 이등병) 입니다.'
-print(sentence)
-sentence2 = "니넨 전역까지 몇일 남았냐~~~? ㅋㅋㅋ"
-print(sentence2)
-sentence3 = """
-고구마호박이 아니라,
-호박고구마에요
-"""
-print(sentence3)
+if (jumin[7] == 1):
+    sex = "남자"
+elif (jumin[7] == 2):
+    sex = "여자"
 
-jumin = "000000-111111"
-
-print("성별 : " + jumin[7])
+print("성별 : " + sex)
 print("연 : " + jumin[0:2])
 print("월 : "+ jumin[2:4])
 print("일 : " + jumin[4:6])
 print("생년월일 : " + jumin[:6])
-print("출생지혁조합번호 : " + jumin[8:12])
+print("출생지역조합번호 : " + jumin[8:12])
 print("출생지역의 같은 성씨 출생 신고순번 : " + jumin[12])
 print("오류검증번호 : " + jumin[13])
 
@@ -27,7 +18,7 @@ python = "Python is Amazing"
 print(python.lower())
 print(python.upper())
 print(python[0].isupper())
-print(len(python))
+print(len(python))  #띄어쓰기 포함
 print(python.replace("Python", "Minecraft"))
 
 index = python.index("n")
@@ -35,8 +26,10 @@ print(index)
 index = python.index("n", index +1)
 print(index)
 
-print(python.find("Java"))
-#print(pythonindex("Java"))
+print(python.find("Java"))  #있으면 0, 없으면 -1
+
+#print(python.index("Python"))  #파이썬으로 하면 오류가 안나고 Java로하면 substring not found 오류가 뜸
+
 print("hi")
 
 print(python.count("n"))
