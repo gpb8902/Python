286059c015453ae85f2dd5ab41ed43e0b5a25ba7
gpb8902 <gpb8902@gmail.com>
2020-08-06 22:15:21 +0900

    Update 20200806.py

---
+++ 20200806.py
@@ -0,0 +1,11 @@
+print("백문이 불여일견\n백견이 불여일타")
+
+print("저는 \"李登鉼(이등병)\" 입니다.")
+
+print("C:\\Users\\Nadocoding\\Destop\\PythonWorkspace>")
+
+print("Red apple\rPine")
+
+print("Redd\bapple")
+
+print("Red\tApple")
