b5abe963543582c86dfc8ccbc2c68d89f7ba8e9e
gpb8902 <gpb8902@gmail.com>
2020-08-06 22:28:01 +0900

    Update 20200806.py

--- 20200806.py
+++ 20200806.py
@@ -9,3 +9,11 @@ print("Red apple\rPine")
 print("Redd\bapple")
 
 print("Red\tApple")
+
+url = "http://naver.com"
+my_str = url.replace("http://","")
+#print(my_str)
+my_str = my_str[:my_str.index(".")] 
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀 번호는 {1}입니다.".format(url, password))
