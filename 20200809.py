6668395170fc32925365129414c27801b453099f
gpb8902 <gpb8902@gmail.com>
2020-08-09 18:49:02 +0900

    Create 20200809.py

---
+++ 20200809.py
@@ -0,0 +1,16 @@
+menu = ("스파게티", "크림치즈스파게티", "로제 파스타", "브리또", "불고기피자", "고구마피자", "치즈피자")
+print(menu[0])
+print(menu[1])
+print(menu[2])
+print(menu[3])
+print(menu[4])
+print(menu[5])
+print(menu[6])
+
+name = "싸와디캅!!!"
+age = "Unknown"
+hobby = "Saying 싸와디캅!!!"
+print(name, age, hobby)
+
+(name, age, hobby) = ("싸와디캅!!!", "Unknown", "Saying 싸와디캅!!!")
+print(name, age, hobby)
