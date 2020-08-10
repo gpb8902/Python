20148528c62267f6c3ece0b057aea2baee3466a2
gpb8902 <gpb8902@gmail.com>
2020-08-10 11:48:26 +0900

    Python 복습

--- 20200523.py
+++ 20200523.py
@@ -1,3 +1,15 @@
+# ------------------------------------------- 문자열, 숫자형 자료형 ------------------------------------------- #
+
+print('풍선') #'풍선'이라고 출력된다. 하지만, '풍선'에서 ''<=이것
+ #은 생략된 채로 출력된다.
+
+print(132) #'132'라고 출력됨, 하지만 1,2,0,3...와 같은 정수를 출력할 때 큰 따옴표나 작은 따옴표는 생략한다.
+
+print("12" + "12")
+ #1212라고 출력된다.
+a = 2  #변수로 ex: print(a *(곱하기임)3)을 출력하면 6으로 출력된다.
+print(a*3)
+
 print ("M: I like Spanish. So, I decided to learn Spanish.")
 print ("W: Hey! And")
 print ("M: So, I learn Spanish by myself, but it is too hard to learn it")
@@ -5,43 +17,14 @@ print ("M: So, I learn Spanish by myself, but it is too hard to learn it")
 print ("I really want to play minecraft every day!!")
 print ('나는 원숭이다.')
 
-a=2
-b=3
-c=5
-d=-2
-e=-3
-f=-5
-g=7
-h=11
-i=13
-j=-7
-k=-11
-l=-13
-m=17
-n=19
-o=23
-p=-17
-q=-19
-r=-23
-s=29
-t=31
-u=37
-v=-29
-w=-31
-x=-37
-y=41
-z=-41
-
-print (a+b+c-d+e-f+g-h+i+-j+k-l+m-n+o-p+q-r+s-t+u-v+w-x+y-z)
-
 x=2
 y=5
 
-print ((x*y*3-22)*8)
+print ((x*y*3-22)*8) #2*5= 10, 10*3= 30, 30-22= 8, 8*8= 64이므로 64로 출력 됨
 
 x=10
-print ((x+5)*3-4)
+print ((x+5)*3-4) # 10+5= 15, 15*3 = 45, 45-4= 41이므로 41로 출력됨
 
-# print (x=4)
+#x=4
 
-print (4*x+2)
+print (4*x+2) #4* 10= 40, 40+2=42 이므로 42로 출력된다. 하지만 위에 28번째 줄에서, #을 지우면18로 출력된다.
--- 20200727.py
+++ 20200727.py
@@ -1,21 +1,28 @@
-banana = 7
+#-----------------불리언---------------------------------------------------------------------------
+#불리언은 True(참) 또는 False(거짓)으로 나눠져있다.
 
-# 참 / 거짓
 
-print(5 > 10)
-print(5 < 10)
-print(True)
-print(35 == 26)
-print(8902 == 8902)
-print('villager' == 'pillager')
-print('크림이' == '크림이')
+print(5<10) #10은 5보다 크므로 True 라고 출력된다.
+print(5>10) #5는 10보다 작은데 여기선 크다고 나와있어 False로 출력된다.
+
+print(35 == 26) #35는 26과 같지 않으므로 False로 출력된다.
+print(8902 == 8902) #8902는 8902와 같기 때문에 True로 출력된다.
+
+
+print("크림이" == "크림이") # ==은 같다라는 뜻이고 크림이 == 크림이는 참이므로 True가 출력된다.
+print('villager' == 'pillager') #villager은 pillager이랑 같지 않으므로 False가 출력된다. 
 print('Spain' == 'France')
 print('남자' == '여자')
 print('이등병' == '이등병')
+
+
+banana = 7 #banana라는 변수가 7이랑 같다. 
+
+print(banana == 7) #변수는 따옴표를 붙이지 않고, banana라는 변수는 7과 같기 때문에 True라고 출력된다.
+
 print('내 나이는' + str(banana) + '살이야.')
 
 
-print(banana == 7)
 
-banana = input('바나나의 개수:')
-print(7 > int(banana))
+banana = input('바나나의 개수:')    #바나나의 개수를 입력받음(문자열 변수 banana)
+print(7 > int(banana))  #계산을 위해서 문자열 변수 banana를 정수형 변수로 변환 후 자신이 바나나를 7보다 작게 입력했으면 True로, 같거나 더 클때는 False로 출력된다.
