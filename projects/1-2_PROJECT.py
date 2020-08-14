ec25a968fa986f04c7eb7972339f1f48cf77858b
gpb8902 <gpb8902@gmail.com>
2020-08-14 22:25:39 +0900

    Update 1-2_PROJECT.py

---
+++ projects/1-2_PROJECT.py
@@ -0,0 +1,27 @@
+중학교 = input("중학교가 다음 중 어디에요?(월봉, 봉산, 기타)")
+No school = ["월봉", "봉산"]
+if 중학교 == "월봉" and middle school in school
+    print("월봉중이네요.")
+elif 중학교 == "봉산" and middle school in school:
+    break
+    print("봉산중이네요.")
+else :
+    중학교 == input("중학교가 어디에요?(월계, 비아, 첨단 또는 천곡)")
+    No school = ["월계", "비아"]
+if 중학교 == "월계" and middle school in No school:
+    break
+    print("월계중이네요.")
+elif 중학교 == "비아" and middle school in No school:
+    break
+    print("비아중이네요.")
+    
+else:
+    중학교 == (input("중학교가 어디에요?(첨단, 천곡)"))
+    No school = ["첨단", "천곡"]
+if 중학교 == "첨단" and middle school in No school:
+    break
+    print("첨단중이네요.")
+elif 중학교 == "천곡" and middle school in No school:
+    print("천곡중이네요")
+else:
+    print("해당 중학교가 이 동네에 존재하지 않아요.")
