0708f4ef60368a125d910ce587fb83d3201c09d3
gpb8902 <gpb8902@gmail.com>
2020-08-17 16:15:09 +0900

    Update 4-3_튜플.py

--- 4-3_튜플.py
+++ 4-3_튜플.py
@@ -1,16 +1,16 @@
-menu = ("스파게티", "크림치즈스파게티", "로제 파스타", "브리또", "불고기피자", "고구마피자", "치즈피자")
-print(menu[0])
-print(menu[1])
-print(menu[2])
-print(menu[3])
-print(menu[4])
-print(menu[5])
-print(menu[6])
+menu = ("스파게티", "크림치즈스파게티", "로제 파스타", "브리또", "불고기피자", "고구마피자", "치즈피자") #menu가 스파게티 ,크림치즈스파게티, 로제...로 지정된다.
+print(menu[0]) #스파게티가 출력된다.
+print(menu[1]) #크림치즈스파게티가 출력된다.
+print(menu[2]) #로제 파스타가 출력된다.
+print(menu[3]) #브리또가 출력된다.
+print(menu[4]) #불고기피자가 출력된다.
+print(menu[5]) #고구마피자가 출력된다.
+print(menu[6]) #치즈피자
 
-name = "싸와디캅!!!"
-age = "Unknown"
-hobby = "Saying 싸와디캅!!!"
-print(name, age, hobby)
+name = "싸와디캅!!!" #name이 싸와디캅!!!으로 지정된다.
+age = "Unknown" #age가 Unknown으로 된다.
+hobby = "Saying 싸와디캅!!!" #hobby가 Saying 싸와디캅!!!으로 된다.
+print(name, age, hobby) #싸와디캅!!!, Unknown, Saying 싸와디캅이 출력된다.
 
-(name, age, hobby) = ("싸와디캅!!!", "Unknown", "Saying 싸와디캅!!!")
-print(name, age, hobby)
+(name, age, hobby) = ("싸와디캅!!!", "Unknown", "Saying 싸와디캅!!!") #name, age, hobby가 싸와디캅!!!, Unknown, Saying 싸와디캅 순으로 지정된다.
+print(name, age, hobby) #싸와디캅!!!, Unknown, Saying 싸와디캅이 출력된다.
