630b8aceac6dfe300f9aa4732db67f73da887ada
gpb8902 <gpb8902@gmail.com>
2020-08-18 22:30:13 +0900

    Update 5-3_while문.py

--- 5-3_while문.py
+++ 5-3_while문.py
@@ -1,67 +1,22 @@
-customer = input("이름이 어떻게 되세요??")
-index = 5
+customer = "모쏠인" #customer가 모쏠인으로 지정된다.
+index = 5 #5가 index가 된다. 
 
-while index >= 1:
-    print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다. {1}번 남았어요.".format(customer, index))
-    index -= 1
+while index >= 1: #1보다 크거나 같을 때까지 진행
+    print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다. {1}번 남았어요.".format(customer, index)) #0이 customer, 1이 index가 된다.
+    index -= 1 #출력될때마다 index가 1씩 작아진다.
 
-person = "Unknown"
+person = "Unknown" #Person이 Unknown이다.
 
-while person != customer :
-        print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다.".format(customer))
-        person == input("이름이 어떻게 되세요? ")
+while person ! == customer : #person이 customer가 될때까지 한다.
+        print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다.".format(customer)) #0이 customer가 된다.
+        person = input("이름이 어떻게 되세요? ") #이름이 어떻게 되세요 라고 묻는다.
 
+if index == 0: #index가 0으로 지정된다.
+    print("커피는 이제 저희가 마시도록 하겠습니다.") #커피는 이제 저희가 마시도록 하겠습니다. 라고 출력된다.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if index == 0:
-    print("커피는 이제 저희가 마시도록 하겠습니다.")
-
-customer = "지구상에 있는 모든 기업들을 인수하신 위대한"
-index = 1
-while True:
-    print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다. 호출 {1}회".format(customer, index))
-    index += 1
+customer = "지구상에 있는 모든 기업들을 인수하신 위대한" #customer이 지구상에 있는 모든 기업들을 인수하신 위대한 으로 지정된다.
+index = 1 #index가 1이 된다.
+while True: #참이 될때까지 한다.
+    print("{0} 고객님, 주문하신 캡틴 아메리카노가 준비되었습니다. 호출 {1}회".format(customer, index)) #0이 customer, 1이 index가 된다.
+    index += 1 #index가 1씩 늘어난다.
 
