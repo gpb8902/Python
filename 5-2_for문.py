9a746bca21cf7654c4200e2d0f17fe1f899fd57e
gpb8902 <gpb8902@gmail.com>
2020-08-18 20:33:37 +0900

    Update 5-2_for문.py

--- 5-2_for문.py
+++ 5-2_for문.py
@@ -1,3 +1,3 @@
-starbucks = ["스타벅스를 인수하신 위대한", "삼성을 인수하신 위대한", "SBS회사를 인수하신 위대한"]
-for customer in starbucks:
-    print("{0} 고객님, 주문하신 커피가 준비되었습니다.".format(customer))
+starbucks = ["스타벅스를 인수하신 위대한", "삼성을 인수하신 위대한", "SBS회사를 인수하신 위대한"] #starbucks가 스타벅스를 인수하신 위대한, 삼성을 인수하신 위대한, SBS회사를 인수하신 위대한이 된다.
+for customer in starbucks: #starbucks에 있는 고객에 customer가 추가된다.
+    print("{0} 고객님, 주문하신 커피가 준비되었습니다.".format(customer)) #스타벅스를...와 삼성... 등등이 출력된다.
