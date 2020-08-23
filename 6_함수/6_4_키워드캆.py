de41356a402e1f6d7b5d5bbc7cae2a7383b813bb
gpb8902 <gpb8902@gmail.com>
2020-08-24 00:36:46 +0900

    Create 6_4_키워드캆.py

---
+++ 6_함수/6_4_키워드캆.py
@@ -0,0 +1,5 @@
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="Muchim Doraji", main_lang="한국어, 영어, 스페인어, 포르투갈어, 아랍어", age=166528)
+profile(main_lang="아랍어, 벵골어, 헝가리어, 베트남어, 러시아어", age=23987178, name="Muchim Gaji")
