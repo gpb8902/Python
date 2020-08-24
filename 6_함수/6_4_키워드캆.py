3a80b0a1763371e19c8d8d6469488ace8e897020
gpb8902 <gpb8902@gmail.com>
2020-08-24 23:21:06 +0900

    Update 6_4_키워드캆.py

--- 6_함수/6_4_키워드캆.py
+++ 6_함수/6_4_키워드캆.py
@@ -1,5 +1,5 @@
-def profile(name, age, main_lang):
-    print(name, age, main_lang)
+def profile(name, age, main_lang): #profile(name, age, main_lang)라는 함수가 생성된다.
+    print(name, age, main_lang) #name, age, main_lang[전체로 지정된 변수]가 출력된다.
 
-profile(name="Muchim Doraji", main_lang="한국어, 영어, 스페인어, 포르투갈어, 아랍어", age=166528)
-profile(main_lang="아랍어, 벵골어, 헝가리어, 베트남어, 러시아어", age=23987178, name="Muchim Gaji")
+profile(name="Muchim Doraji", main_lang="한국어, 영어, 스페인어, 포르투갈어, 아랍어", age=166528) #name="Muchim Doraji", main_lang="한국어, 영어, 스페인어, 포르투갈어, 아랍어", age=166528로 지정된다.
+profile(main_lang="아랍어, 벵골어, 헝가리어, 베트남어, 러시아어", age=23987178, name="Muchim Gaji") #main_lang="아랍어, 벵골어, 헝가리어, 베트남어, 러시아어", age=23987178, name="Muchim Gaji"로 출력된다.
