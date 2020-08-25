da14538bc1579c444044bd0c5848269f2ea1f0f0
gpb8902 <gpb8902@gmail.com>
2020-08-25 23:55:56 +0900

    Update 4-4_세트(나중에 다시).py

--- 4_ 자료구조/4-4_세트(나중에 다시).py
+++ 4_ 자료구조/4-4_세트(나중에 다시).py
@@ -3,8 +3,23 @@
 my_set = {1,2, 3, 3, 3}
 print(my_set)
 
-java = {"유재석", "양세형", "하도권"}
+java = {"유재석", "양세형", "하도권", "하하"}
 python = set(["유재석", "김종국"])
 
 #Java, Python을 모두 할 수 있는 개발자
 print(java & python)
+print(java.intersection(python))
+
+print(java | python)
+print(java.union(python))
+
+print(java - python)
+print(java.difference(python))
+print(python - java)
+print(python.difference(java))
+
+python.add("양세형", "하하")
+print(python)
+
+java.remove("하하")
+print(java)
