0b7d2f8b1db3c7d07df258dfbf4d5bf0ece5b406
gpb8902 <gpb8902@gmail.com>
2020-08-27 23:58:16 +0900

    Update 7_1_표준입출력.py

--- 7_입출력/7_1_표준입출력.py
+++ 7_입출력/7_1_표준입출력.py
@@ -13,4 +13,11 @@ print("Python", "Java", file=sys.stderr)
 
 scores = {"수학":85, "영어":100, "코딩":100}
 for subject, score in scores.items():
-    print(subject.ljust(15), str(score).rjust(5))
+    print(subject.ljust(15), str(score).rjust(5), sep=":")
+
+for num in range(1, 1000):
+    print("대기번호 : " + str(num).zfill(3))
+
+answer = input("다음중 당신이 가장 좋아하는 호러캐릭터는?(Granny, Bendy, Bardy, Siren Head, Cartoon cat) : ")
+print("당신이 좋아하는 호러캐릭터는 " + answer + "(이)군요." + answer + " is so scary that I'll have nightmare.")
+print(type(answer))
