5c82b01209ab326df508f88a4e729cd9c6efe44e
gpb8902 <gpb8902@gmail.com>
2020-08-27 23:39:58 +0900

    Update 7_1_표준입출력.py

--- 7_입출력/7_1_표준입출력.py
+++ 7_입출력/7_1_표준입출력.py
@@ -6,3 +6,9 @@ print("Python", "Java", sep=", ", end="? ")
 print("Which programing is funnier, Python or Java?")
 print("Python", "Java", sep=", ")
 print("Which programing is funnier, Python or Java?")
+
+import sys
+print("Python", "Java", file=sys.stdout)
+print("Python", "Java", file=sys.stderr)
+
+scores = {"수학":85, "영어":100, "코딩":100}
