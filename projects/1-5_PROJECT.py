a2e9163dd130d29195587fdccf9a295a382ed90d
gpb8902 <gpb8902@gmail.com>
2020-08-30 20:15:12 +0900

    Update 1-5_PROJECT.py

--- projects/1-5_PROJECT.py
+++ projects/1-5_PROJECT.py
@@ -1,18 +1,24 @@
 import time
 
 rn = input("이름이 무엇입니까?")
+time.sleep(2)
 print(rn + "님, 안녕하세요.")
 inf = input("당신 전 학교는 어디입니까?")
+time.sleep(3)
 print(inf + "이라...")
 ainf = input("그럼 생일이 언제입니까?")
+time.sleep(2)
 print(ainf +"이군요!!")
 ainff = input("당신은 어떤 언어를 배우고 싶습니까?(Python 같은 거 말고 한국어, 영어 같은 언어)")
+time.sleep(6)
 print(ainff + "를 배우고 싶군요!! 부럽습니다. 저도" + ainff + "을(를) 배우고 싶군요!")
 ainfff = input("당신은 애완동물을 기른가요?(네, 또는 아니요로 대답)")
+time.sleep(4)
 
 if ainfff == "네":
     print("정말로요?")
     ainffff = input("그럼 당신은 어떤 동물을 기르고 있나요?")
+    time.sleep(3)
     if ainffff == "고양이":
         print("참으로 귀엽군요!!!")
     
