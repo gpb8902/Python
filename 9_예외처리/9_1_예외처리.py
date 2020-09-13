163c532e1ed9d10575d2383266662e6a2fc36ba4
gpb8902 <gpb8902@gmail.com>
2020-09-13 23:56:05 +0900

    Update 9_1_예외처리.py

--- 9_예외처리/9_1_예외처리.py
+++ 9_예외처리/9_1_예외처리.py
@@ -3,9 +3,11 @@ try:
     nums = []
     nums.append(int(input("첫번째 수를 입력하세요 : ")))
     nums.append(int(input("두번째 수를 입력하세요 : ")))
-    nums.append(int(nums[0] / nums[1], nums[2]))
+    #nums.append(int(nums[0] / nums[1]))
     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
 except ValueError:
     print("There is an Error irritating you!!")
 except ZeroDivisionError as err:
     print(err)
+except:
+    print("There is an Unknown Error")
