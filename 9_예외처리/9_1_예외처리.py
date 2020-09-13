754e0810793ee4efe5abac8affa41a8b412d5616
gpb8902 <gpb8902@gmail.com>
2020-09-14 00:02:11 +0900

    Update 9_1_예외처리.py

--- 9_예외처리/9_1_예외처리.py
+++ 9_예외처리/9_1_예외처리.py
@@ -1,13 +1,9 @@
 try:
-    print("나누기 전용 계산기입니다.")
-    nums = []
-    nums.append(int(input("첫번째 수를 입력하세요 : ")))
-    nums.append(int(input("두번째 수를 입력하세요 : ")))
-    #nums.append(int(nums[0] / nums[1]))
-    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+    print("오직 한자리 수만 나눌 수 있는 계산기 입니다.")
+    num1: int(input('첫번째 수를 입력하세요 : '))
+    num2 = int(input("두번째 수를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10:
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
 except ValueError:
-    print("There is an Error irritating you!!")
-except ZeroDivisionError as err:
-    print(err)
-except:
-    print("There is an Unknown Error")
+    print("잘못된 값을 입력하였습니다.")
