03510643b9ef382b05f6321edfbfae0d185c1e44
gpb8902 <gpb8902@gmail.com>
2020-09-13 23:54:06 +0900

    Create 9_1_예외처리.py

---
+++ 9_예외처리/9_1_예외처리.py
@@ -0,0 +1,11 @@
+try:
+    print("나누기 전용 계산기입니다.")
+    nums = []
+    nums.append(int(input("첫번째 수를 입력하세요 : ")))
+    nums.append(int(input("두번째 수를 입력하세요 : ")))
+    nums.append(int(nums[0] / nums[1], nums[2]))
+    print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
+except ValueError:
+    print("There is an Error irritating you!!")
+except ZeroDivisionError as err:
+    print(err)
