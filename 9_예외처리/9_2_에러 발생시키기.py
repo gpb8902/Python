d5c8f22ef46e5db63e6229b663c6cab94935bda2
gpb8902 <gpb8902@gmail.com>
2020-09-14 22:53:16 +0900

    Update 9_2_에러 발생시키기.py

--- 9_예외처리/9_2_에러 발생시키기.py
+++ 9_예외처리/9_2_에러 발생시키기.py
@@ -1,1 +1,9 @@
-print(12)
+try:
+    print("한 자리 수만 나눌 수 있는 전용 계산기 입니다.")
+    num1 = int(input("첫번째 수를 입력하세요 : "))
+    num2 = int(input("두번째 수를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10:
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 수만 입력 할 수 있습니다.")
