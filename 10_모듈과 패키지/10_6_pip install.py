696057eee4833df05bce6deee7631c00c6ef17a3
gpb8902 <gpb8902@gmail.com>
2020-09-18 00:08:19 +0900

    Update 10_6_pip install.py

--- 10_모듈과 패키지/10_6_pip install.py
+++ 10_모듈과 패키지/10_6_pip install.py
@@ -1,3 +1,6 @@
-import inspect
-import random
-print(inspect.getfile(random))
+from base64 import *
+print("Hello")
+
+from bs4 import BeautilfulSoup
+soup = BeautilfulSoup("<p>some<b>bad<i>HTML")
+print(soup.prettify)
