5a7572e6177fcd0f42675ba91d4d8daf35eabfae
gpb8902 <gpb8902@gmail.com>
2020-09-30 22:25:25 +0900

    Update 3_create_frame.py

--- 실습_게임/기본/pygame_basic/3_create_frame.py
+++ 실습_게임/기본/pygame_basic/3_create_frame.py
@@ -10,13 +10,19 @@ pygame.display.set_caption("Stick figure's battle")
 
 background = pygame.image.load("E:\\Python\\para.png")
 
-character = pygame.image.load("E:\\Python\\lysis.png")
+character = pygame.image.load("E:\\Python\\test.png")
 character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
-character_x_pos = screen_wdith / 2
-character_y_pos = screen_height
+character_x_pos = screen_width / 2
+character_y_pos = screen_height - character_height
 
+character1 = pygame.image.load("E:\\Python\\test1.png")
+character1_size = character1.get_rect().size
+character1_width = character1_size[0]
+character1_height = character1_size[1]
+character1_x_pos = screen_width / 4
+character1_y_pos = screen_height - character_height
 
 running = True
 while running:
@@ -25,8 +31,9 @@ while running:
             running = False
             
     screen.blit(background, (0, 0))
-    
+
     screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(character1, (character1_x_pos, character1_y_pos))
 
     pygame.display.update()
 
